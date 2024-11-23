3f518002b96d2acfd53decb3bb7990367f98d338
Rediet Tedila <rediettedila5@gmail.com>
2024-11-23 07:54:29 -0800

    Update role.controller.js
    Update role.controller.js

--- Broker_System_Backend/controllers/role.controller.js
+++ Broker_System_Backend/controllers/role.controller.js
@@ -16,9 +16,6 @@ const createRole = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
-
-
-
 const getAllRoles = async (req, res) => {
   try {
     const roles = await Role.find();
